1346679e2aca4fdb4cb0f6d12f7a191f2f1f2655
villva-vinoth <vinothpukal6580@gmail.com>
2024-04-05 13:54:24 +0530

    header route

--- src/components/whyTorus/Award/awardData.js
+++ src/components/whyTorus/Award/awardData.js
@@ -1,8 +1,19 @@
-import image1 from '../../Assets/Why Torus/partner/image 13.png'
-import image2 from '../../Assets/Why Torus/partner/image 14.png'
-import image3 from '../../Assets/Why Torus/partner/image 15.png'
-import image4 from '../../Assets/Why Torus/partner/image 17.png'
-import image5 from '../../Assets/Why Torus/partner/image 18.png'
+import image1 from '../../Assets/Why Torus/partner/climatelaunchpad.png'
+import image2 from '../../Assets/Why Torus/partner/drro.png'
+import image3 from '../../Assets/Why Torus/partner/edi.png'
+import image4 from '../../Assets/Why Torus/partner/flotd.png'
+import image5 from '../../Assets/Why Torus/partner/forge.png'
+import image6 from '../../Assets/Why Torus/partner/hexgn.png'
+import image7 from '../../Assets/Why Torus/partner/idex.png'
+import image8 from '../../Assets/Why Torus/partner/innovation.png'
+import image9 from '../../Assets/Why Torus/partner/nandan.jpg'
+import image10 from '../../Assets/Why Torus/partner/sine.png'
+import image11 from '../../Assets/Why Torus/partner/srm.png'
+import image12 from '../../Assets/Why Torus/partner/startupindia.png'
+import image13 from '../../Assets/Why Torus/partner/unido.png'
+import image14 from '../../Assets/Why Torus/partner/vjtitbi.png'
+// import image15 from '../../Assets/Why Torus/partner/'
+
 
 
 const award =[
@@ -20,7 +31,43 @@ const award =[
     },
     {
         img:image5,
+    },
+    {
+        img:image6,
     }
+    ,
+    {
+        img:image7,
+    }
+    ,
+    {
+        img:image8,
+    }
+    ,
+    {
+        img:image9,
+    }
+    ,
+    {
+        img:image10,
+    }
+    ,
+    {
+        img:image11,
+    }
+    ,
+    {
+        img:image12,
+    }
+    ,
+    {
+        img:image13,
+    }
+    ,
+    {
+        img:image14,
+    }
+   
 ]
 
 export {award}
